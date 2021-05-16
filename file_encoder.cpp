aeb63ad7ba8c42e5aed376a290ee6b5155283c40
alireza <alireza.gerami4030@gmail.com>
2021-05-17 01:35:59 +0430

    encode file function added.

--- file_encoder.cpp
+++ file_encoder.cpp
@@ -42,6 +42,100 @@ void encode_block(const encoder_t & encoder,
     std::ofstream & out_stream,
     char * data_buffer_,
     char * fec_buffer_,
+    const std::size_t & read_amount);
+void encode_file(const encoder_t &  encoder, const std::string & input_filename,const  std::string & output_filename);
+
+int main() {
+
+    const std::string input_filename = "input.txt";
+    const std::string output_filename = "encoded_file.txt";
+
+    const std::size_t field_descriptor = 8;
+    const std::size_t generator_polynomial_index = 120;
+    const std::size_t generator_polynomial_root_count = 32;
+
+    const schifra::galois::field field(field_descriptor,
+        schifra::galois::primitive_polynomial_size06,
+        schifra::galois::primitive_polynomial06);
+    schifra::galois::field_polynomial generator_polynomial(field);
+
+    if (
+        !schifra::make_sequential_root_generator_polynomial(field,
+            generator_polynomial_index,
+            generator_polynomial_root_count,
+            generator_polynomial)
+    ) {
+        std::cout << "Error - Failed to create sequential root generator!" << std::endl;
+        return 1;
+    }
+
+    const encoder_t encoder(field, generator_polynomial);
+    encode_file(encoder, input_filename, output_filename);
+    return 0;
+}
+
+void encode_file(const encoder_t &  encoder, const std::string & input_filename, const std::string & output_filename) {
+    const int one_chunk_size_bytes = 1048546;
+    char * data_buffer_ = new char[data_length];
+    char * fec_buffer_ = new char[fec_length];
+    char * chunk_data;
+    std::size_t file_size = schifra::fileio::file_size(input_filename);
+    if (file_size == 0) {
+        std::cout << "Error: input file has ZERO size." << std::endl;
+        return;
+    }
+    std::cout << "Size of the file is" << " " << file_size << " " << "bytes" << std::endl;
+
+    std::ifstream in_stream(input_filename.c_str(), std::ios::binary);
+    if (!in_stream) {
+        std::cout << "Error: input file could not be opened." << std::endl;
+        return;
+    }
+
+    std::ofstream out_stream(output_filename.c_str(), std::ios::binary);
+    if (!out_stream) {
+        std::cout << "Error: output file could not be created." << std::endl;
+        return;
+    }
+
+    for (int chunk = 0; chunk <= file_size / one_chunk_size_bytes; chunk++) {
+        int length = 0;
+        std::size_t remaining_bytes = 0;
+
+        if (file_size - (chunk * one_chunk_size_bytes) >= one_chunk_size_bytes) {
+            length = one_chunk_size_bytes;
+            remaining_bytes = length;
+        } else {
+            length = file_size - (chunk * one_chunk_size_bytes);
+            remaining_bytes = length;
+        }
+
+        chunk_data = new char[length];
+        in_stream.read(chunk_data, length);
+        int index = 0;
+        while (remaining_bytes >= data_length) {
+            for (int i = 0; i < data_length; i++, index++) {
+                data_buffer_[i] = chunk_data[index];
+            }
+            encode_block(encoder, out_stream, data_buffer_, fec_buffer_, data_length);
+            remaining_bytes -= data_length;
+        }
+        if (remaining_bytes > 0) {
+            for (int i = 0; i < remaining_bytes; i++, index++) {
+                data_buffer_[i] = chunk_data[index];
+            }
+            encode_block(encoder, out_stream, data_buffer_, fec_buffer_, remaining_bytes);
+        }
+        printf("chunk %d : %d bytes done!\n", chunk, length);
+    }
+    in_stream.close();
+    out_stream.close();
+
+}
+void encode_block(const encoder_t & encoder,
+    std::ofstream & out_stream,
+    char * data_buffer_,
+    char * fec_buffer_,
     const std::size_t & read_amount) {
 
     for (std::size_t i = 0; i < read_amount; ++i) {
@@ -65,89 +159,4 @@ void encode_block(const encoder_t & encoder,
 
     out_stream.write( & data_buffer_[0], static_cast < std::streamsize > (read_amount));
     out_stream.write( & fec_buffer_[0], fec_length);
-}
-
-int main() {
-    
-    const std::string input_filename = "input.txt";
-    const std::string output_filename = "encoded_file.txt";
-
-    const int one_chunk_size_bytes = 1048546;
-    char * data_buffer_ = new char[data_length];
-    char * fec_buffer_ = new char[fec_length];
-    char * chunk_data;
-
-    const std::size_t field_descriptor = 8;
-    const std::size_t generator_polynomial_index = 120;
-    const std::size_t generator_polynomial_root_count = 32;
-
-    const schifra::galois::field field(field_descriptor,
-        schifra::galois::primitive_polynomial_size06,
-        schifra::galois::primitive_polynomial06);
-    schifra::galois::field_polynomial generator_polynomial(field);
-
-    if (
-        !schifra::make_sequential_root_generator_polynomial(field,
-            generator_polynomial_index,
-            generator_polynomial_root_count,
-            generator_polynomial)
-    ) {
-        std::cout << "Error - Failed to create sequential root generator!" << std::endl;
-        return 1;
-    }
-
-    const encoder_t encoder(field, generator_polynomial);
-
-    std::size_t file_size = schifra::fileio::file_size(input_filename);
-    if (file_size == 0) {
-        std::cout << "Error: input file has ZERO size." << std::endl;
-        return 1;
-    }
-    std::cout << "Size of the file is" << " " << file_size << " " << "bytes" << std::endl;
-
-    std::ifstream in_stream(input_filename.c_str(), std::ios::binary);
-    if (!in_stream) {
-        std::cout << "Error: input file could not be opened." << std::endl;
-        return 1;
-    }
-
-    std::ofstream out_stream(output_filename.c_str(), std::ios::binary);
-    if (!out_stream) {
-        std::cout << "Error: output file could not be created." << std::endl;
-        return 1;
-    }
-    
-    for (int chunk = 0; chunk <= file_size / one_chunk_size_bytes; chunk++) {
-        int length = 0;
-        std::size_t remaining_bytes = 0;
-       
-        if (file_size - (chunk * one_chunk_size_bytes) >= one_chunk_size_bytes) {
-            length = one_chunk_size_bytes;
-            remaining_bytes = length;
-        } else {
-            length = file_size - (chunk * one_chunk_size_bytes);
-            remaining_bytes = length;
-        }
-
-        chunk_data = new char[length];
-        in_stream.read(chunk_data, length);
-        int index = 0;
-        while (remaining_bytes >= data_length) {
-            for (int i = 0; i < data_length; i++, index++) {
-                data_buffer_[i] = chunk_data[index];
-            }
-            encode_block(encoder, out_stream, data_buffer_, fec_buffer_, data_length);
-            remaining_bytes -= data_length;
-        }
-        if (remaining_bytes > 0) {
-            for (int i = 0 ; i < remaining_bytes; i++, index++) {
-                data_buffer_[i] = chunk_data[index];
-            }
-            encode_block(encoder, out_stream, data_buffer_, fec_buffer_, remaining_bytes);
-        }
-        printf("chunk %d : %d bytes done!\n", chunk, length);
-    }
-    in_stream.close();
-    out_stream.close();
-    return 0;
 }
